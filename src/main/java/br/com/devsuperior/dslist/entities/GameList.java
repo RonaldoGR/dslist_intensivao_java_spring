b110cfd112faa8b3d62195400e21b33ca348a019
Ronaldo Rocha <ronaldogandrarocha@gmail.com>
2025-08-20 16:22:44 -0300

    feat: created domain model

---
+++ src/main/java/br/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,35 @@
+package br.com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="tb_gamelist")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
